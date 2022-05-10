680d98d99dc2222907536a7eea663b90aad9fa4e
adjong87 <adjong87@gmail.com>
2022-05-10 20:00:01 +0200

    opdracht twee goed end op weg

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
     const [aambeienCounter, setAambeienCounter] = useState(0)
     const [appelCounter, setAppelCounter] = useState(0)
     const [kiwiCounter, setKiwiCounter] = useState(0)
+    const [voornaam, setVoornaam] = useState('')
+    const [achternaam, setAchternaam] = useState('')
+    const [leeftijd, setLeeftijd] = useState('')
+    const [postcode, setPostcode] = useState('')
+    const [bezorgFrequentie, setBezorgFrequentie] = useState('')
+    const [bezorgMoment, setBezorgMoment] = useState('')
+
 
     function handleReset() {
         setBananaCounter(0);
@@ -18,21 +25,21 @@ function App() {
     return (
         <>
             <h1>Fruitmand bezorgservice</h1>
-            <span><p>Aambeien</p>
-        <button
-            type="button"
-            onClick={() => setAambeienCounter(aambeienCounter - 1)}
-            disabled={aambeienCounter === 0}
-        >
-            -
-        </button>
-                {aambeienCounter}
-                <button
-                    type="button"
-                    onClick={() => setAambeienCounter(aambeienCounter + 1)}>
-            +
-        </button>
-        </span>
+            <p>Aambeien</p>
+            <button
+                type="button"
+                onClick={() => setAambeienCounter(aambeienCounter - 1)}
+                disabled={aambeienCounter === 0}
+            >
+                -
+            </button>
+            {aambeienCounter}
+            <button
+                type="button"
+                onClick={() => setAambeienCounter(aambeienCounter + 1)}>
+                +
+            </button>
+
             <p>Bananen</p>
             <button
                 type="button"
@@ -77,12 +84,76 @@ function App() {
                 onClick={() => setKiwiCounter(kiwiCounter + 1)}>
                 +
             </button>
+
             <button
                 type="button"
                 onClick={handleReset}>
                 reset
             </button>
 
+
+            <form>
+                <label htmlFor="voornaam">Voornaam</label>
+                <input type="text"
+                       id="voornaam"
+                       value={voornaam}
+                       onChange={(e) => setVoornaam(e.target.value)}/>
+                <label htmlFor="achternaam">Achternaam</label>
+                <input type="text"
+                       id="achternaam"
+                       value={achternaam}
+                       onChange={(e) => setAchternaam(e.target.value)}/>/>
+                <label htmlFor="leeftijd">Leeftijd</label>
+                <input type="number"
+                       id="leeftijd"
+                       placeholder="0"
+                       value={leeftijd}
+                       onChange={(e) => setLeeftijd(e.target.value)}/>/>
+                <label htmlFor="postcode">Postcode</label>
+                <input type="text"
+                       id="postcode"
+                       value={postcode}
+                       onChange={(e) => setPostcode(e.target.value)}/>/>
+                <label htmlFor="bezorgfrequentie">Bezorgfrequentie</label>
+                <select name="bezorgfrequentie"
+                        id="bezorgfrequentie"
+                        value={bezorgFrequentie}
+                        onChange={(e) => setBezorgFrequentie(e.target.value)}/>>
+                    <option value="wekelijks">Iedere week</option>
+                    <option value="tweewekelijks">Iedere twee weken</option>
+                    <option value="maandelijks">Iedere maand</option>
+                </select>
+                <span>
+                    <input type="radio"
+                           id="overdag"
+                           name="bezorgmoment"
+                           value="overdag">
+                    </input>
+                    <label htmlFor="overdag">Overdag</label>
+                    <input type="radio"
+                           id="avond"
+                           name="bezorgmoment"
+                           value="avond">
+                    </input>
+                    <label htmlFor="avond">'s avonds</label>
+                </span>
+                <label htmlFor="comment">Opmerkingen</label>
+                <textarea id="comment"
+                          name="comment">
+                    Plaats hier eventuele opmerkingen
+                </textarea>
+                <span>
+                    <input type="checkbox"
+                           id="akkoord"/>
+                    <label htmlFor="akkoord">Ik ga akkoord met de voorwaarden</label>
+                </span>
+
+            </form>
+            <button type="submit"
+                    id="submit"
+                    value="submit">
+                Verzend
+            </button>
         </>
     );
 }
